48407d6fd326bbfd6fd25e2847e85ea679e773c6
abdoemi <MAHJOUBI.ABDESSAMAD@student.emi.ac.ma>
2023-05-17 10:45:22 +0100

    Initial commit

---
+++ Desktop/app-maven-DevOps/src/main/java/com/example/appmavendevops/AppMavenDevOpsApplication.java
@@ -0,0 +1,14 @@
+package com.example.appmavendevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMavenDevOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMavenDevOpsApplication.class, args);
+        System.out.println("Bonjour");
+    }
+
+}
---
+++ Desktop/app-maven-DevOps/src/test/java/com/example/appmavendevops/AppMavenDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appmavendevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMavenDevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
